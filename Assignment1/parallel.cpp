d1f6c04449c0f749dbbb9f6f80d3152e27d40685
JacopoRaffi <raffijacopo@gmail.com>
2023-03-02 04:05:57 -0500

    Parallel solution(need to be corrected)

--- Assignment1/parallel.cpp
+++ Assignment1/parallel.cpp
@@ -3,12 +3,18 @@
 #include <vector>
 #define N 20
 
+//shared resources
+std::vector<int> v(N);
+std::vector<int> w(N, 0);
+
+
 int f(int value){
     return (2 * value) + 1;
 }
 
-void job(std::vector<int> v, std::vector<int> w){
-
+void job(int start, int end){
+    for(int i = start; i < end; i++)
+        w[i] = f(v[i]); 
 }
 
 void printVector(std::vector<int> vect){
@@ -18,7 +24,7 @@ void printVector(std::vector<int> vect){
     std::cout << "\n";
 }
 
-
+//for simplicity we assume that N is a multiple of numThreads
 int main(int argc, char* argv[]){
     uint32_t numThreads = (argc == 1) ? 4 : atoi(argv[1]);
 
@@ -28,14 +34,22 @@ int main(int argc, char* argv[]){
     }
     std::srand(time(nullptr));
 
-    std::vector<int> v(N);
     for(int i = 0; i < N; i++){
         v[i] = std::rand() % 10;
     }
+    printVector(v);
+    //every thread take a portion of vector from i to i+size
+    uint32_t size = N / numThreads;  
 
-    std::vector<std::thread> threads(numThreads);
+    std::vector<std::thread> threads;
+    int start = 0, end = size;
     for(int i = 0; i < numThreads; i++){
-        threads.push_back(std::thread(job, v));
+        end = start + size;
+        threads.push_back(std::thread(job, start, end));
+        start += size;
     }
-
+    printVector(w);
+    //for(int i = 0; i < numThreads; i++)
+        
+        //threads[i].join();
 }
