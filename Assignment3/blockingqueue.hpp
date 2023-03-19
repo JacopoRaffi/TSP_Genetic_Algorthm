6d374cc9746523e1dbeda1f17f46821e85307d53
JacopoRaffi <raffijacopo@gmail.com>
2023-03-19 19:35:02 +0100

    correct blockingqueue

--- Assignment3/blockingqueue.hpp
+++ Assignment3/blockingqueue.hpp
@@ -9,9 +9,14 @@
 template<typename T>
 class blockingqueue{
 public:
+    blockingqueue(){
+        m = new std::mutex();
+        condition = new std::condition_variable();
+    }
+
     T pop(){
-        std::lock_guard<std::mutex>(m);
-        condition.wait(m, [this]() {return empty; });
+        std::lock_guard<std::mutex>(*m);
+        condition->wait(*m, [this]() {return empty; });
 
         T value = q.front();
         q.pop();
@@ -21,16 +26,16 @@ public:
     }
 
     void push(T& value){
-        std::lock_guard<std::mutex>(m);
+        std::lock_guard<std::mutex>(*m);
         q.push(value);
         empty = false;
-        condition.notify_all();
+        condition->notify_all();
     }
 
 private:
     std::queue<T> q;
-    std::mutex m;
-    std::condition_variable condition;
+    std::mutex *m;
+    std::condition_variable *condition;
     bool empty = true;
 };
 
