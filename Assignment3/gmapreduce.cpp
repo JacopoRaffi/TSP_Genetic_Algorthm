670a0e837b2d803b28deff7292d27b55af3b697d
JacopoRaffi <raffijacopo@gmail.com>
2023-03-19 18:54:40 +0100

    Creating mappers part

--- Assignment3/gmapreduce.cpp
+++ Assignment3/gmapreduce.cpp
@@ -1,5 +1,47 @@
 #include <thread>
 #include <iostream>
+#include <vector>
+#include <map>
+#include <string>
+#include <future>
+#include <cmath>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+pair<string, int> map_f(string word){
+    return make_pair(word, 1);
+}
+
+int hash_to_reducer(string word, int dim){
+    hash<string> hash_fun;
+
+    return hash_fun(word) % dim;        
+}
+
+void mapper(int start, int end, vector<string>& words, int reducers){
+    //first transform words in a vector of pair
+    vector<pair<string, int>> out_map(end - start);
+    transform(words.begin()+start, words.begin()+end, out_map.begin(), map_f);
+    map<string, int> pairs;
+
+    //mini-reduce so i avoid to calculate the same hash for equal Keys
+    for(int i = 0; i < out_map.size(); i++){
+        string word = out_map[i].first;
+        if(pairs.contains(word)){
+            pairs[word] += 1;
+        }
+        else{
+            pairs[word] = 1;
+        }
+    }
+    //send pair <word, value> to reducer
+    for(pair<string, int> p : pairs){
+        //todo: insert in reducers queue
+        int index = hash_to_reducer(p.first, reducers);
+    }
+}
 
 /* TODO:
  * mappers take a portion of input vector;
@@ -13,7 +55,36 @@
  * it could be useful to create a blocking-queue;
 */
 
+int main(int argc, char* argv[]){
+    if(argc < 3)
+        cout << "At least 2 arguments!\n";
 
-int main(int argc, char* arv[]){
-    
+    uint8_t mappers = atoi(argv[1]);
+    uint8_t reducers = atoi(argv[2]);
+    vector<string> words;
+
+    for(int i = 3; i < argc; i++){ //words starts from third argument
+        words.push_back(argv[i]); 
+    }
+
+    vector<thread> map_pool; 
+    int size;
+    uint32_t dim = words.size();
+    if(mappers <= dim)
+        size = ceil((float)dim / (float)mappers); 
+    else
+        size = 1;
+
+    int start = 0, end;
+    for(int i = 0; i < min((int)dim, (int)mappers); i++){
+        if((start + size) > (dim - 1)){
+            end = dim;
+        }
+        else
+            end = start + size;
+            
+        map_pool.push_back(thread(mapper, start, end, ref(words)));
+        start += size;
+    }
+    vector<future<pair<string, int>>> reduce_pool;
 }
