0f93abb9dda0d11036cce28c55fbcd755263756f
JacopoRaffi <raffijacopo@gmail.com>
2023-03-19 19:21:20 +0100

    Simplified version of blocking dequeue

---
+++ Assignment3/blockingqueue.hpp
@@ -0,0 +1,37 @@
+#ifndef BLOCKINGQUEUE_H
+#define BLOCKINGQUEUE_H
+
+//a simplified blocking queue (max length is "infinite")
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+template<typename T>
+class blockingqueue{
+public:
+    T pop(){
+        std::lock_guard<std::mutex>(m);
+        condition.wait(m, []() {return empty; });
+
+        T value = q.front();
+        q.pop();
+        empty = q.empty();
+
+        return value;
+    }
+
+    void push(T& value){
+        std::lock_guard<std::mutex>(m);
+        q.push(value);
+        empty = false;
+        condition.notify_all();
+    }
+
+private:
+    std::queue<T> q;
+    std::mutex m;
+    std::condition_variable condition;
+    bool empty = true;
+};
+
+#endif
