9e81e65c0f03af5b2d744ed784ae0e517140de1b
JacopoRaffi <raffijacopo@gmail.com>
2023-06-02 09:55:05 +0200

    Add data file

--- Project/src/tsp_main.cpp
+++ Project/src/tsp_main.cpp
@@ -1,24 +1,46 @@
 #include <vector> 
 #include <iostream>
 #include <random>
+#include <sstream>
 #include <limits>
+#include <fstream>
 
 #include "./tsp_classes/tsp_seq.hpp"
 
 using namespace std;
 
-//using Graph = vector<vector<double>>; //Adjiacency Matrix (directed graph) 
+vector<pair<double, double>> read_coord_file(string file){
+    ifstream read_file(file);
+    string coordinates;
+    stringstream parser;
 
-Graph graph_init(int size, int seed){
-    Graph g(size);
-    default_random_engine gen;
-    gen.seed(seed);
-    uniform_real_distribution<double> unif(1, 200); 
+    vector<pair<double, double>> cities;
 
-    for(int i = 0; i < size; i++){
-        g[i] = vector<double>(size);
-        for(int j = 0; j < size; j++){
-            g[i][j] = unif(gen);
+    while(getline(read_file, coordinates)){
+        parser.str(coordinates);
+        string x, y;
+        parser >> x >> y;
+        cities.push_back(make_pair(stod(x), stod(y)));
+
+        parser.clear();
+    }
+
+    return cities;
+}
+
+inline double euclidean_distance(pair<double, double> coord_a, pair<double, double> coord_b){
+    return sqrt(pow(coord_b.first - coord_a.first, 2) + pow(coord_b.second - coord_a.second, 2));
+}
+
+Graph graph_init(vector<pair<double, double>>& cities, int seed){ 
+    Graph g(cities.size());
+
+    //lower triangular matrix (un-directed graph) so I save, more or less, half space 
+    //Start from 1 because I exclude the diagonal
+    for(int i = 1; i < 10; i++){ //10 is just for test in my pc
+        g[i] = vector<double>(i);
+        for(int j = 0; j < i; j++){
+            g[i][j] = euclidean_distance(cities[i], cities[j]);
         }
     }
     return g;
@@ -27,12 +49,12 @@ Graph graph_init(int size, int seed){
 
 int main(int argc, char *argv[]){
     if(argc < 6){
-        cerr << "Usage: " << argv[0] << " workers vertexes iterations population mode mutationRate(optional) crossoverRate(optional) seed(optional) start(optional)\n";
+        cerr << "Usage: " << argv[0] << " workers file iterations population mode mutationRate(optional) crossoverRate(optional) seed(optional) start(optional)\n";
         return -1;
     }
     
     int workers = atoi(argv[1]);
-    int size = atoi(argv[2]);
+    string file_coordinates = argv[2];
     int generations = atoi(argv[3]);
     int population_size = atoi(argv[4]);
     string mode = argv[5];
@@ -57,7 +79,15 @@ int main(int argc, char *argv[]){
         start_vertex = atoi(argv[10]);
     }
     
-    Graph g = graph_init(size, seed);
+    vector<pair<double, double>> cities = read_coord_file("../cities.txt");
+    Graph g = graph_init(cities, seed);
+
+    for(int i = 1; i < 10; i++){
+        for(int j = 0; j < i; j++){
+            cout << g[i][j] << " ";
+        }
+        cout << "\n";
+    }
 
     if(mode == "sq"){ //sequential mode
         TSPSeq tsp(g, population_size, start_vertex);
